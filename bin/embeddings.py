4559d6572c2ad90053af3ec503453781f8bf80b0
kvetab <brazdilovak@gmail.com>
2021-11-24 19:49:33 +0100

    Create embeddings for both datasets

--- bin/embeddings.py
+++ bin/embeddings.py
@@ -6,9 +6,8 @@ from embeddings_reproduction import embedding_tools
 
 def embed_sequences(data, model_path, out_path, k=5, overlap=False):
     embeds_data = []
-    split = data["Antibody"].str.split()
-    heavy = [remove_excess(seqs[0]) for seqs in split]
-    light = [remove_excess(seqs[1]) for seqs in split]
+    heavy = data["heavy"]
+    light = data["light"]
     for seqlist in [heavy, light]:
         embeds = embedding_tools.get_embeddings(model_path, seqlist, k, overlap)
         embeds_data.append(pd.DataFrame.from_records(embeds))
@@ -27,36 +26,28 @@ def remove_excess(seq):
     return seq.replace(",", "")
 
 
-MODEL_DIRECTORY = "../embed_models"
+DATA_DIR = "../data"
+MODEL_DIRECTORY = "../data/embed_models"
 MODELS = [mod_file for mod_file in os.listdir(MODEL_DIRECTORY) if mod_file.endswith(".pkl")]
 
-DATA_TRAIN = pd.read_csv("data/chen_train_data.csv", sep=";")
-DATA_VALID = pd.read_csv("data/chen_valid_data.csv", sep=";")
-DATA_TEST = pd.read_csv("data/chen_test_data.csv", sep=";")
+
+CHEN_DATA = pd.read_csv(os.path.join(DATA_DIR, "chen/deduplicated/chen_data.csv"))
+TAP_DATA = pd.read_csv(os.path.join(DATA_DIR, "tap/TAP_data.csv"))
 
 
-def embed_data(model, data_train, data_test, data_valid):
+def embed_data(model, data, label):
     print(f"Creating embeddings with model {model}.")
     mod_name = model.split(".")[0]
     k = int(mod_name.split("_")[1])
     w = int(mod_name.split("_")[2])
-    # train
-    outfile = f"data/embeddings/embeddings_train_{mod_name}.ftr"
+    # 
+    outfile = f"../data/{label}/embeddings/embeddings_{label}_{mod_name}.ftr"
     if not os.path.isfile(outfile):
-        df = embed_sequences(data_train, f"../embed_models/{model}", outfile, k=k)
-        print(f"Writing embeddings to file {outfile}.")
-    # validation
-    outfile = f"data/embeddings/embeddings_valid_{mod_name}.ftr"
-    if not os.path.isfile(outfile):
-        df = embed_sequences(data_valid, f"../embed_models/{model}", outfile, k=k)
-        print(f"Writing embeddings to file {outfile}.")
-    # test
-    outfile = f"data/embeddings/embeddings_test_{mod_name}.ftr"
-    if not os.path.isfile(outfile):
-        df = embed_sequences(data_test, f"../embed_models/{model}", outfile, k=k)
+        df = embed_sequences(data, os.path.join(MODEL_DIRECTORY, model), outfile, k=k)
         print(f"Writing embeddings to file {outfile}.")
 
         
 if __name__ == "__main__":
     for model in MODELS: 
-        embed_data(model, DATA_TRAIN, DATA_TEST, DATA_VALID)
+        embed_data(model, CHEN_DATA, "chen")
+        embed_data(model, TAP_DATA, "tap")
